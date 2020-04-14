7bd8fbf760c2b948af0fdf56ecdee57e26b11d89
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-04-14 13:24:33 -0700

    Organized Java Projects into separate folders

---
+++ JavaProjects/Fundementals/ArrayList/ArrayList1/WordStorer.java
@@ -0,0 +1,34 @@
+package ArrayList;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordStorer
+{
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String input;
+        ArrayList<String> stringList = new ArrayList<String>();
+        System.out.println("Please enter a strings separated by a space. Then type [0] at the end when finished");
+
+        do {
+            input = reader.next();
+            if (!input.equals("[0]")) {
+                stringList.add(input);
+                System.out.println(input + " added to list.");
+
+            }
+        } while (!input.equals("[0]"));
+
+        printList(stringList);
+    }
+
+    private static void printList(ArrayList<String> stringList){
+        int place = 0;
+        System.out.println("List of Strings:");
+        while(place < stringList.size()){
+            System.out.println(Integer.toString(place + 1) + ") " + stringList.get(place));
+            place++;
+        }
+    }
+
+}
